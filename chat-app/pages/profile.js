bcf68b7d96bcbd2e22c9df08522a66075c27806f
labhk <labh.k2003@gmail.com>
2023-11-01 01:10:02 +0530

    UI fix

--- chat-app/pages/profile.js
+++ chat-app/pages/profile.js
@@ -10,10 +10,10 @@ export default function Profile() {
       <ResponsiveAppBar />
       
       <div className="h-screen mt-10 flex flex-col items-center justify-center ">
-        <h1 className='text-4xl text-track-blue font-bold mb-3'>My Profile</h1>
-        <p className='text-track-blue text-xl'>Name: <span className='text-black'>{session?.user?.name}</span></p>
-        <p className='text-track-blue text-xl mb-5'>Email: <span className='text-black'>{session?.user?.email}</span></p>
-        <p className='mb-5'><img src={session?.user?.image} height={150} style={{ borderRadius: "100px"}}/></p>
+        <h1 className='text-[4rem] text-track-blue font-bold mb-6'>My Profile</h1>
+        <p className='text-track-blue text-[2rem]'>Name: <span className='text-black'>{session?.user?.name}</span></p>
+        <p className='text-track-blue text-[2rem] mb-5'>Email: <span className='text-black'>{session?.user?.email}</span></p>
+        <p className='mb-5'><img src={session?.user?.image} className='h-24' height={250} style={{ borderRadius: "100px"}}/></p>
         <button className={styles.btn}><a className={styles.linkOut} href='/api/auth/signout'>Sign Out</a></button>
         <br/>
         <button className={styles.btn}><a className={styles.linkOut} href='/'>Back to Home</a></button>
